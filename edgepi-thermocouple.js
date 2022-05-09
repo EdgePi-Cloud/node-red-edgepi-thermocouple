93e6793db28250680ef88bef783d01547f932488
flavius-t <flavius.t@hotmail.com>
2022-05-10 00:48:48 +0100

    add edgepi-thermocouple node

---
+++ edgepi-thermocouple.js
@@ -0,0 +1,60 @@
+module.exports = function(RED) {
+    const spawn = require('child_process').spawn;
+
+    // file_path to edgepi-thermocouple bash script
+    const executablePath = __dirname + '/edgepi-thermocouple'
+
+    sampleCommand = 2;
+
+    function ThermocoupleNode(config) {
+        RED.nodes.createNode(this, config);
+        const node = this;
+
+        function inputlistener(msg, send, done) {
+            if (node.child != null) {
+                // send input to child process (Py script)
+                node.child.stdin.write(sampleCommand+"\n", () => {
+                    if (done) { done(); }
+                });
+                node.status({fill:"green",shape:"ring",text:"connected to child"});
+            }
+            else {
+                // logs error to Node-RED's console.
+                node.error(RED._("edgepi-thermocouple.errors.pythoncommandnotfound"), msg);
+                node.status({fill:"red",shape:"ring",text:"disconnected from child"});
+            }
+        }
+
+        // creates child process instance which will run command located at executablePath
+        // with the argument 2 (single-shot sample).
+        node.child = spawn(executablePath, [sampleCommand]);
+
+        // called on input to this node
+        node.on("input", inputlistener);
+
+        // listen to output from child process
+        node.child.stdout.on('data', function (data) {
+            node.log("out: " + data + " :");
+        });
+
+        node.child.stderr.on('data', function (data) {
+            node.log("err: "+ data + " :");
+        });
+
+        // handle exit from child process
+        node.child.on("close", function(data) {
+            node.child = null;
+            node.finished();
+        });
+
+        // handle exit from parent process
+        node.on("close", function(done) {
+            if (node.child != null) {
+                node.child.stdin.write("closing");
+                node.child.kill('SIGKILL');
+            }
+            else { done(); }
+        });
+    }
+    RED.nodes.registerType("edgepi-thermocouple-node", ThermocoupleNode);
+}
